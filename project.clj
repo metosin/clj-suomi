f138639ab225fd59cd168c2d3f506b6b855852eb
Juho Teperi <juho.teperi@iki.fi>
2015-09-21 23:54:09 +0300

    Fix project.clj url and description

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
 (defproject metosin/clj-suomi "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
+  :description "Access Finnish code sets"
+  :url "https://github.com/metosin/clj-suomi"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.7.0"]])

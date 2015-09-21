f4f79acb728ddf137bee38119102684f650fb865
Juho Teperi <juho.teperi@iki.fi>
2015-09-22 02:51:16 +0300

    Try magical coveralls script

--- scripts/coveralls.sh
+++ scripts/coveralls.sh
@@ -1,3 +1,7 @@
 #!/bin/bash
 COVERALLS_URL='https://coveralls.io/api/v1/jobs'
-curl -F 'json_file=@target/coverage/coveralls.json' "$COVERALLS_URL"
+
+json=$(cat target/coverage/coveralls.json)
+json="${json::-1}, \"service_pull_request\": \"${CI_PULL_REQUEST}\", \"repo_token\": \"${COVERALLS_REPO_TOKEN}\"}"
+
+echo $json | curl -F 'json_file=@-' "$COVERALLS_URL"

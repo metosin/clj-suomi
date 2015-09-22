976a935e13168495252fd75680443c88d4a0b1f7
Juho Teperi <juho.teperi@iki.fi>
2015-09-22 16:45:52 +0300

    Disable ftp tests

--- test/clj_suomi/utils/ftp_test.clj
+++ test/clj_suomi/utils/ftp_test.clj
@@ -7,13 +7,13 @@
                :user "anonymous"
                :pass ""})
 
-(deftest test-list-file-names
+(comment
   (with-open [conn (client test-ftp)]
     (is (clojure.set/subset?
           #{"README" "dev" "favicon.ico" "ftp" "incoming" "index" "pub" "rfc"}
           (set (list-file-names conn))))))
 
-(deftest is-closed-test
+(comment
   (let [conn (client test-ftp)]
     (with-open [conn conn]
       (is (.isConnected conn)))
